1f737c129235b10e03360503390294f175ba2a61
leibori <45977036+leibori@users.noreply.github.com>
2018-12-26 00:43:25 +0200

    add PrintCommand

---
+++ PrintCommand.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ori on 12/25/18.
+//
+
+#include <iostream>
+#include "PrintCommand.h"
+void PrintCommand:: execute(){
+        cout << this->toPrint << endl;
+}
---
+++ PrintCommand.h
@@ -0,0 +1,30 @@
+//
+// Created by ori on 12/25/18.
+//
+
+#ifndef PROJ1_PRINTCOMMAND_H
+#define PROJ1_PRINTCOMMAND_H
+
+#include <string>
+
+#include "Expression.h"
+#include "Command.h"
+
+
+using namespace std;
+
+class PrintCommand : public Command {
+    string toPrint;
+public:
+    explicit PrintCommand(Expression *ePrint) {
+        this->toPrint = to_string(ePrint->calculate());
+    }
+
+    explicit PrintCommand(string sPrint) { this->toPrint = sPrint; }
+
+
+    void execute();
+};
+
+#endif //PROJ1_PRINTCOMMAND_H
+
